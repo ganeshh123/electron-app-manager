b16fe8a99182a5841c5b23a6a632a3842922fd32
Ganesh H <43777439+ganeshh123@users.noreply.github.com>
2020-08-18 22:17:44 +0100

    folder delete utility

---
+++ tests/test_folder_delete.js
@@ -0,0 +1,12 @@
+let fs = require('fs')
+let path = require('path')
+let removeDir = require('../src/renderer/utils/folder_delete')
+
+let targetPath = path.join(__dirname, 'static', '/Code Cache')
+
+let exclusions = [
+    '15f3998ef8d9e98a_0',
+    'f4c8f8a8ec37f4de_0'
+]
+
+removeDir(targetPath, exclusions)
